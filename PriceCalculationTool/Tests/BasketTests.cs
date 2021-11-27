afbb61b079a633ff38be40d8f87dc0a91a71161d
Serkan Sozer <serkan@groovytechs.co.uk>
2021-11-27 13:15:31 +0000

    Scenario 3 implementation

--- PriceCalculationTool/Tests/BasketTests.cs
+++ PriceCalculationTool/Tests/BasketTests.cs
@@ -31,7 +31,7 @@ namespace Tests
         }
 
         [Test]
-        public void BasketShouldCalculateSumWithAnOfferApplied()
+        public void BasketShouldCalculateSumWithButterOfferApplied()
         {
             var basket = new Basket();
             basket.AddItem(_availableProducts.Find("Butter"));
@@ -47,6 +47,25 @@ namespace Tests
             var sum = basket.Sum();
 
             Assert.AreEqual(3.10m, sum);
+        }
+
+        [Test]
+        public void BasketShouldCalculateSumWithMilkOfferApplied()
+        {
+            var basket = new Basket();
+            basket.AddItem(_availableProducts.Find("Milk"));
+            basket.AddItem(_availableProducts.Find("Milk"));
+            basket.AddItem(_availableProducts.Find("Milk"));
+            basket.AddItem(_availableProducts.Find("Milk"));
+
+            foreach (var offer in _activeOffers.Offers)
+            {
+                offer.ApplyToBasket(basket);
+            }
+
+            var sum = basket.Sum();
+
+            Assert.AreEqual(3.45m, sum);
         }
 
     }
